130068ba97490ca43d3851cc13bce337b314d397
Zyurs <romain.partouche@gmail.com>
2017-12-30 00:41:08 +0100

    Add support for two types of Youtube channel URLs

--- src/yt2rss.bash
+++ src/yt2rss.bash
@@ -1,5 +1,12 @@
 #!/bin/bash
 
 youtube_channel=$1;
-rss_flux=`curl -s $youtube_channel | egrep -o 'https://.+channel_id=[0-9a-zA-Z_]+'`;
+rss_flux="Not available";
+if grep -q channel <<< $youtube_channel;
+then
+  rss_flux="https://www.youtube.com/feeds/videos.xml?channel_id=`egrep -o 'channel/[0-9a-Z_]+' <<< $youtube_channel | cut -d / -f 2`";
+elif grep -q user <<< $youtube_channel;
+then
+  rss_flux=`curl -s $youtube_channel | egrep -o 'https://.+channel_id=[0-9a-Z_]+'`;
+fi;
 echo $rss_flux;
