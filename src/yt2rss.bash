498df61fa194f369f6264a145242dead97e8138a
Zyurs <zyurs@users.noreply.github.com>
2018-07-30 20:18:37 +0200

    Add '-' in channel id

--- src/yt2rss.bash
+++ src/yt2rss.bash
@@ -4,9 +4,9 @@ youtube_channel=$1;
 rss_flux="Not available";
 if grep -q channel <<< $youtube_channel;
 then
-  rss_flux="https://www.youtube.com/feeds/videos.xml?channel_id=`egrep -o 'channel/[0-9a-Z_]+' <<< $youtube_channel | cut -d / -f 2`";
+  rss_flux="https://www.youtube.com/feeds/videos.xml?channel_id=`egrep -o 'channel/[0-9a-Z_-]+' <<< $youtube_channel | cut -d / -f 2`";
 elif grep -q user <<< $youtube_channel;
 then
-  rss_flux=`curl -s $youtube_channel | egrep -o 'https://.+channel_id=[0-9a-Z_]+'`;
+  rss_flux=`curl -s $youtube_channel | egrep -o 'https://.+channel_id=[0-9a-Z_-]+'`;
 fi;
 echo $rss_flux;
